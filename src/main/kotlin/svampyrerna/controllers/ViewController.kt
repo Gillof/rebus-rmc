4ce3254c4f0bd347bc856e76fde59accc68a33e2
Jakob Horndahl <jakob.horndahl@mojang.com>
2021-03-17 14:33:02 +0100

    Add feature to ping itself every 10m to ensure uptime on heroku

--- src/main/kotlin/svampyrerna/controllers/ViewController.kt
+++ src/main/kotlin/svampyrerna/controllers/ViewController.kt
@@ -1,20 +1,30 @@
 package svampyrerna.controllers
 
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.scheduling.annotation.Scheduled
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule
 import io.micronaut.views.View
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.slf4j.LoggerFactory
 import java.util.*
 
 @Tag(name = "Views")
 @Secured(SecurityRule.IS_ANONYMOUS)
 @Controller("/")
-class ViewController {
+class ViewController(
+    @Client(id = "ping") private val pingClient: HttpClient,
+    @Value("\${ping.enabled:false}") private val pingEnabled: Boolean
+) {
+    private val LOG = LoggerFactory.getLogger(ViewController::class.java)
+
     @Produces(MediaType.TEXT_HTML)
     @Get
     @View("home")
@@ -39,5 +49,16 @@ class ViewController {
     @View("auth")
     fun authFailed(): Map<String, Any> {
         return Collections.singletonMap<String, Any>("errors", true)
+    }
+
+    @Scheduled(fixedRate = "10m")
+    fun everyFiveMinutes() {
+        if (pingEnabled) {
+            LOG.info("Pinging /health to ensure uptime")
+            val response = pingClient.toBlocking().exchange<Any>("/health")
+            LOG.info("Got ${response.status} from /health")
+        } else {
+            LOG.debug("Ping disabled")
+        }
     }
 }
