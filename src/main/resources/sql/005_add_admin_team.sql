225fd5e66d8bc82402bcb8a6e05479bdb4f547e1
Ictis <34275614+Gillof@users.noreply.github.com>
2023-04-08 20:43:51 +0200

    Update adding of admin role

--- src/main/resources/sql/005_add_admin_team.sql
+++ src/main/resources/sql/005_add_admin_team.sql
@@ -1,1 +1,5 @@
-INSERT INTO teams (id, name, secret, role) VALUES ('aa48d0d1-4db5-4588-abcb-42cd408c0687', 'admin', 's3cr3t', 'ADMIN') ON CONFLICT (name) DO NOTHING;
+INSERT INTO teams (id, name, secret, role) 
+SELECT 'aa48d0d1-4db5-4588-abcb-42cd408c0687', 'admin', 's3cr3t', 'ADMIN'
+WHERE NOT EXISTS (
+	SELECT name FROM teams WHERE name='admin'
+);
